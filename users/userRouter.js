b3f82542295321d73d3d5507b4816f51c4d0ac5e
std <std@admins-Mac-mini.local>
2019-12-04 14:50:38 -0800

    completed work in userRouter.js file

--- users/userRouter.js
+++ users/userRouter.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 // Import database access
 const udb = require('./userDb');
+const pdb = require('../posts/postDb');
 
-
+// ********************************************************
+// ********************************************************
 router.post('/', validateUser, (req, res) => {
   // do your magic!
   udb.insert(req.body)
@@ -22,10 +24,28 @@ router.post('/', validateUser, (req, res) => {
   })
 });
 
-router.post('/:id/posts', (req, res) => {
+
+// ********************************************************
+// ********************************************************
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const body = req.body;
+  body.user_id = req.params.id;
+  pdb.insert(body)
+    .then(post=>{
+      // console.log(post);
+      res.status(200).json(post);
+    })
+    .catch(err=>{
+      console.log("Error in pdb.insert in POST /users/:id/posts");
+      res.status(500)
+        .json({error: "Could not add new post to database."});
+    })
 });
 
+
+// ********************************************************
+// ********************************************************
 router.get('/', (req, res) => {
   // do your magic!
   udb.get()
@@ -39,25 +59,83 @@ router.get('/', (req, res) => {
     })
 }); //End of router.get
 
+
+// ********************************************************
+// ********************************************************
 router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
   res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+
+// ********************************************************
+// ********************************************************
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  udb.getUserPosts(req.params.id)
+    .then(posts=>{
+      // console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch(err=>{
+      console.log("Error in udb.getUserPosts in GET /users/:id/posts");
+      res.status(500)
+        .json({error: "User's posts could not be retrieved."});
+    });
 });
 
-router.delete('/:id', (req, res) => {
+
+// ********************************************************
+// ********************************************************
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  udb.remove(req.params.id)
+    .then(count=>{
+      // console.log(count);
+      if(count===1) {
+        res.status(200).json(`User ${req.user.name} has been deleted`);
+      }
+      else {
+        console.log("Error in udb.remove in DELETE /users/:id");
+        res.status(500)
+          .json({error: "User could not be deleted."});
+      }
+    })
+    .catch(err=>{
+      console.log("Error in udb.remove in DELETE /users/:id");
+      res.status(500)
+        .json({error: "User could not be deleted."});
+    });
 });
 
-router.put('/:id', (req, res) => {
+
+// ********************************************************
+// ********************************************************
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
+  udb.update(req.params.id,req.body)
+    .then(count=>{
+      // console.log(count);
+      if(count===1) {
+        res.status(200).json(`User with id ${req.user.id} has been updated`);
+      }
+      else {
+        console.log("Error in udb.update in PUT /users/:id");
+        res.status(500)
+          .json({error: "User could not be updated."});
+      }
+    })
+    .catch(err=>{
+      console.log("Error in udb.update in PUT /users/:id");
+      res.status(500)
+        .json({error: "User could not be updated."});
+    });
 });
 
-//custom middleware
 
+// ********************************************************
+// custom middleware
+// ********************************************************
 function validateUserId(req, res, next) {
   // do your magic!
   udb.getById(req.params.id)
@@ -87,12 +165,26 @@ function validateUser(req, res, next) {
   } 
   else if(!body.name) {
     res.status(400).json({ message: "missing required name field" });
+  } 
+  else {
+    next();
   }
-  next();
+  
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const body = req.body;
+  // console.log(body);
+  if(Object.keys(body).length === 0) {
+    res.status(400).json({ message: "missing post data" });
+  } 
+  else if(!body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } 
+  else {
+    next();
+  }
 }
 
 module.exports = router;
